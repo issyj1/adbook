54613d41f6788c6fa86822215f6f17b77f747dd0
issyj1 <issyjay@hotmail.co.uk>
2026-05-19 16:36:32 +0100

    your message

--- src/App.js
+++ src/App.js
@@ -146,16 +146,24 @@ const imgRef = useRef(null);
    
 </div>
     </div>
+    <img className="img3" src="/adbook/img/meart.jpg" alt="Me" />
 
-   
+    <ExpandableIntro fullText={`Hi, I'm Isabelle (or Issy) and I'm a creative.
+I’ve been lucky enough to work with some of the business's biggest brands and awarded agencies including Mother, Saatchi & Saatchi and BETC Paris, plus running my own creative studio for the past 2 years.
+
+I’m a cultural sponge who aspires to Jack Dee’s sense of humour and always carries a Sakura Pigma Micron 02 fineliner. In my previous life before advertising I worked at Vogue Paris which was full of very serious people, so I decided to be silly for a living. I have a sharp radar for bullshit – especially my own.
+Having graduated from UCL, I speak multiple languages and also work as a photographer inside and outside of adland.
+I attended School of Communication Arts 2.0 2019/20 with a scholarship.`} />
+
 {/* Example: slow-scrolling photos */}
 
      
 
       {/* ------------------ Footer Images ------------------ */}
+      <div className="h2-container">
 
       <h4>My other sites:</h4>
-
+</div>
       <a href="https://isabellejohnsonphoto.com" target="_blank" rel="noopener noreferrer">
     <img className="links" src="/adbook/img/Assets/photo.png" alt="ij" />
 </a>
@@ -166,7 +174,6 @@ const imgRef = useRef(null);
       
 <p className="footer">Bespoke build by Isabelle Johnson at newworldcreative.studio </p>
     </div>
-    
   );
 }
 
